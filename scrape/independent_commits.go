2f307e81ac5a9e2a13b97465a10a2eca22808014
ZHENK <chengzhenyang@gmail.com>
2022-06-11 01:16:20 +1200

    Implement merge_base and rev-list scraper for git

---
+++ scrape/independent_commits.go
@@ -0,0 +1,16 @@
+package scrape
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+func CreateIndependentCommits(r *git.Repository, branchAHead *object.Commit, branchBHead *object.Commit) ([]*object.Commit, error) {
+	independent, err := object.Independents([]*object.Commit{branchAHead, branchBHead})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create independent commits: %w", err)
+	}
+	return independent, nil
+}
