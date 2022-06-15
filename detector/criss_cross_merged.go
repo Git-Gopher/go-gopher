c5824c9d12bde5644bd6dfdc97f3b5a28d0a6071
ZHENK <chengzhenyang@gmail.com>
2022-06-16 10:54:54 +1200

    replace tabs with spaces for comment diagram

--- detector/criss_cross_merged.go
+++ detector/criss_cross_merged.go
@@ -5,11 +5,11 @@ import "log"
 // Example of criss cross merge.
 // This usually happen during hotfixes.
 //
-// 			    3a4f5a6 -- 973b703 -- a34e5a1 (branch A)
-// 			  /        \ /
+//          3a4f5a6 -- 973b703 -- a34e5a1 (branch A)
+//        /        \ /
 // 7c7bf85          X
-// 				\        / \
-//  			  8f35f30 -- 3fd4180 -- 723181f (branch B)
+//        \        / \
+//          8f35f30 -- 3fd4180 -- 723181f (branch B)
 
 type CrissCrossBranchInfo struct {
 	Hash string
