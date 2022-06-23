8b2a56810ec1e5bc46f4f34633f2be67a0c7b4e8
ZHENK <chengzhenyang@gmail.com>
2022-06-23 15:51:33 +1200

    Implement rankSimilar and longestSubstring

--- detector/branch_compare.go
+++ detector/branch_compare.go
@@ -1,14 +1,18 @@
 package detector
 
 import (
+	"log"
+	"strings"
+
 	"github.com/Git-Gopher/go-gopher/model/github"
 	"github.com/Git-Gopher/go-gopher/model/local"
 	"github.com/Git-Gopher/go-gopher/violation"
 	"github.com/adrg/strutil"
+	"gopkg.in/vmarkovtsev/go-lcss.v1"
 )
 
 // Branch name.
-type BranchCompareDetect func(branches []MockBranchCompareModel) (bool, violation.Violation, error)
+type BranchCompareDetect func(branches []MockBranchCompareModel) (int, []violation.Violation, error)
 
 // XXX: Move into either local or github model.
 type MockBranchCompareModel struct {
@@ -55,17 +59,27 @@ func NewBranchCompareDetector(detect BranchCompareDetect) *BranchCompareDetector
 	}
 }
 
-// TODO: Github Workflow: Branches must have consistent names.
-// Research: https://stackoverflow.com/questions/29476737/similarities-in-strings-for-name-matching
-// Methods: q-grams, longest common substring and longest common subsequence.
-func NewBranchNameConsistencyDetect() BranchCompareDetect {
-	return func(branches []MockBranchCompareModel) (bool, violation.Violation, error) {
-		// TODO: Do some algorithm to see if the branch names are consistent enough.
-		if len(branches) > 10 {
-			return false, violation.NewCommonViolation("Branch message longer than 10"), nil
+var featureNames = [...]string{"feature", "feat"}
+
+func NewFeatureBranchNameDetect() BranchCompareDetect {
+	return func(branches []MockBranchCompareModel) (int, []violation.Violation, error) {
+		branchRefs := []string{}
+	b:
+		for _, branch := range branches {
+			for _, featureName := range featureNames {
+				if strings.Contains(branch.Ref, featureName) {
+					// contains featureNames part of branch
+					continue b
+				}
+			}
+			// does not contain featureNames
+			branchRefs = append(branchRefs, branch.Ref)
 		}
 
-		return true, nil, nil
+		// TODO: report using warning (not violation)
+		log.Println("branch without feature/feat:", branchRefs)
+
+		return 0, nil, nil
 	}
 }
 
@@ -79,4 +93,13 @@ func rankSimilar(input []string, metric strutil.StringMetric) []float64 {
 		}
 	}
 	return results
+}
+
+func longestSubstring(input []string) string {
+	b := make([][]byte, len(input))
+	for i, str := range input {
+		b[i] = []byte(str)
+	}
+
+	return string(lcss.LongestCommonSubstring(b...))
 }
--- detector/branch_compare_test.go
+++ detector/branch_compare_test.go
@@ -27,7 +27,7 @@ func Test_rankSimilar(t *testing.T) {
 					"feature/test"},
 				metric: metrics.NewLevenshtein(),
 			},
-			want: []float64{0, 0, 0},
+			want: []float64{0.21969696969696972, 1.2727272727272727, 1.3181818181818183, 0.9924242424242425},
 		},
 		{
 			name: "without main",
@@ -38,7 +38,7 @@ func Test_rankSimilar(t *testing.T) {
 					"feature/test"},
 				metric: metrics.NewLevenshtein(),
 			},
-			want: []float64{0, 0, 0},
+			want: []float64{1.2272727272727273, 1.2272727272727273, 0.9090909090909092},
 		},
 		{
 			name: "our repo",
@@ -51,13 +51,63 @@ func Test_rankSimilar(t *testing.T) {
 				},
 				metric: metrics.NewLevenshtein(),
 			},
-			want: []float64{0, 0, 0},
+			want: []float64{0.636302294197031, 0.4784075573549258, 0.5416666666666666, 0.6820175438596492},
+		},
+		{
+			name: "game example",
+			args: args{
+				input: []string{
+					"feat/sql-database",
+					"feature/score-board",
+					"feature/tile-animations",
+					"feature/proof-of-concept",
+				},
+				metric: metrics.NewLevenshtein(),
+			},
+			want: []float64{0.9987604881769641, 1.208905415713196, 1.2010869565217392, 1.125},
+		},
+		{
+			name: "game example",
+			args: args{
+				input: []string{
+					"feat/sql-database",
+					"feature/score-board",
+					"feature/tile-animations",
+					"feature/proof-of-concept",
+					"feature/user-and-leaderboard-controller",
+					"feature/browse-artwork",
+					"feature/manage-artwork",
+				},
+				metric: metrics.NewLevenshtein(),
+			},
+			want: []float64{1.8519073413238172, 2.638975345783126, 2.516583054626533, 2.342948717948718, 2.205128205128205, 2.886477652782001, 2.8089718252761733},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := rankSimilar(tt.args.input, tt.args.metric); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("rankSimilar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_longestSubstring(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"features", args{[]string{"feature/score-board", "feature/tile-animations"}}, "feature/"},
+		{"nothing", args{[]string{"caching-basic", "various-tidy"}}, "-"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestSubstring(tt.args.input); got != tt.want {
+				t.Errorf("longestSubstring() = %v, want %v", got, tt.want)
 			}
 		})
 	}
