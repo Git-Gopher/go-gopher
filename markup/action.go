3281f6a6d5304659ae0d12151bc9660d3c9e29b3
wqsz7xn <wqsz7xn@protonmail.com>
2022-06-28 12:18:42 +1200

    reintroduce sanitize

--- markup/action.go
+++ markup/action.go
@@ -2,6 +2,7 @@ package markup
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Helper functions for GitHub actions commands
@@ -12,5 +13,11 @@ func Group(title, content string) {
 }
 
 func Outputs(name, value string) {
+	// Sanitize with literals
+	name = strings.ReplaceAll(name, "\n", `\n`)
+	value = strings.ReplaceAll(value, "\n", `\n`)
+	name = strings.ReplaceAll(name, "\r", `\r`)
+	value = strings.ReplaceAll(value, "\r", `\r`)
+
 	fmt.Printf("::set-output name=%s::%s\n", name, value)
 }
