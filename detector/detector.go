a85da4a585524f5756a425cef90716a1bcee69c9
wqsz7xn <wqsz7xn@protonmail.com>
2022-08-31 14:09:58 +1200

    fix lint

--- detector/detector.go
+++ detector/detector.go
@@ -49,11 +49,13 @@ func (c *common) IsCurrentPR(pr *remote.PullRequest) bool {
 	// In the case where there is no current branch/pr, default to report all.
 	if c.PR == nil {
 		log.Warn("no pr found")
+
 		return true
 	}
 
 	if pr.Number == c.PR.Number {
 		log.Warnf("current pr number %d", c.PR.Number)
+
 		return true
 	}
 
