c71121ed6a01ae3576ebaaef1c2febdc8aba3b06
wqsz7xn <67990210+wqsz7xn@users.noreply.github.com>
2022-09-25 12:46:39 +1300

    fix empty commit violation, rework diff gen using tree patches (#140)
    * fix empty commit violation, rework diff gen using tree patches
    
    * error type
    
    * fix spell

--- cmd/github-action/main.go
+++ cmd/github-action/main.go
@@ -15,6 +15,13 @@ func main() {
 	app.HelpName = "go-gopher-action"
 	app.Usage = "A github action for analyzing GitHub repositories"
 	app.Version = version.BuildVersion()
+	app.Flags = []cli.Flag{
+		&cli.BoolFlag{
+			Name:     "disable-pr-comment",
+			Required: false,
+			Usage:    "disable pr comment output which cleans up stdout log",
+		},
+	}
 	app.Action = actionCommand
 
 	if err := app.Run(os.Args); err != nil {
