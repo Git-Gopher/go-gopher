475d6ec22c3a0a901e5923c3e9ac50aa17d02f98
ZHENK <chengzhenyang@gmail.com>
2022-10-19 05:08:09 +1300

    Add weighting for the demo

--- workflow/detect.go
+++ workflow/detect.go
@@ -45,12 +45,48 @@ var rulesConfig = []*rule.Runner{
 	// 	TrunkBased: nil,
 	// }),
 
-	rules.NewCherryPickRelease(rule.NewDefaultWeights()),
-	rules.NewCherryPick(rule.NewDefaultWeights()),
-	rules.NewCrissCrossMerged(rule.NewDefaultWeights()),
-	rules.NewFeatureBranching(rule.NewDefaultWeights()),
-	rules.NewHotfix(rule.NewDefaultWeights()),
-	rules.NewUnresolved(rule.NewDefaultWeights()),
+	rules.NewCherryPick(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0.0204560338060376),
+		GitFlow:    rule.NewWeight(0.172141890128531),
+		GitLabFlow: rule.NewWeight(0.0546810891458411),
+		OneFlow:    rule.NewWeight(0.0534825582657873),
+		TrunkBased: rule.NewWeight(0.021875),
+	}),
+	rules.NewCherryPickRelease(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0),
+		GitFlow:    rule.NewWeight(0.0833333333333333),
+		GitLabFlow: rule.NewWeight(0),
+		OneFlow:    rule.NewWeight(0),
+		TrunkBased: rule.NewWeight(0),
+	}),
+	rules.NewCrissCrossMerged(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0),
+		GitFlow:    rule.NewWeight(0),
+		GitLabFlow: rule.NewWeight(0),
+		OneFlow:    rule.NewWeight(0),
+		TrunkBased: rule.NewWeight(0),
+	}),
+	rules.NewFeatureBranching(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0.0934959349593496),
+		GitFlow:    rule.NewWeight(0.344935731059201),
+		GitLabFlow: rule.NewWeight(0.4547008547008540),
+		OneFlow:    rule.NewWeight(0.380150605629752),
+		TrunkBased: rule.NewWeight(0.166666666666667),
+	}),
+	rules.NewHotfix(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0),
+		GitFlow:    rule.NewWeight(0),
+		GitLabFlow: rule.NewWeight(0.04166666667),
+		OneFlow:    rule.NewWeight(0.03062200957),
+		TrunkBased: rule.NewWeight(0),
+	}),
+	rules.NewUnresolved(&rule.Weights{
+		GitHubFlow: rule.NewWeight(0),
+		GitFlow:    rule.NewWeight(0),
+		GitLabFlow: rule.NewWeight(0),
+		OneFlow:    rule.NewWeight(0),
+		TrunkBased: rule.NewWeight(0),
+	}),
 
 	// When just starting out use default weight before calibrating
 	// default weight is 1.0 for all workflows
